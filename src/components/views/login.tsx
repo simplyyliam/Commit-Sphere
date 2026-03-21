c0f7683944c143763c5788a44fe9169f0c64c76f
simplyyliam <simplyliam.dev@gmail.com>
2026-03-21 21:42:56 +0200

    feat: initial commit

---
+++ src/components/views/login.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Login() {
+
+    
+    return (
+        <div className="flex items-center justify-center w-full h-full bg-black/35">
+            
+        </div>
+    )
+}
