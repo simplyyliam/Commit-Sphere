b2082d40673599a11d339e6306eddf76a2860c47
simplyyliam <simplyliam.dev@gmail.com>
2026-03-22 19:47:04 +0200

    test for embed

--- src/components/embed/embed.tsx
+++ src/components/embed/embed.tsx
@@ -1,35 +1,21 @@
-import { useEffect } from "react";
-import axios from "axios";
-import CanvasLayer from "../canvas/canvasLayer";
-import { useCommits } from "@/store/useCommits";
+
 export default function EmbedApp() {
-  const { setCommits } = useCommits();
 
-  useEffect(() => {
-    const username =
-      new URLSearchParams(window.location.search).get("user") || "simplyyliam";
 
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_API_BASE}/embed/${username}`)
-      .then((res) => {
-        console.log("SERVER DATA:", res.data);
-        setCommits(res.data.totalCommits ?? res.data.totalContributions ?? 0);
-      })
-      .catch((err) => console.error(err));
-  }, [setCommits]);
+  // useEffect(() => {
+  //   const username =
+  //     new URLSearchParams(window.location.search).get("user") || "simplyyliam";
+
+  //   axios
+  //     .get(`${import.meta.env.VITE_BACKEND_API_BASE}/embed/${username}`)
+  //     .then((res) => {
+  //       console.log("SERVER DATA:", res.data);
+  //       setCommits(res.data.totalCommits ?? res.data.totalContributions ?? 0);
+  //     })
+  //     .catch((err) => console.error(err));
+  // }, [setCommits]);
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        background: "#010101",
-        position: "fixed",
-        top: 0,
-        left: 0,
-      }}
-    >
-      <CanvasLayer />
-    </div>
+    <div style={{height: "100%", width: "100%" }}>Hello world</div>
   );
 }
